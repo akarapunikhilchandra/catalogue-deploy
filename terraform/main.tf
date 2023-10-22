16837fa4092aeb70be959cc1c0f0bc439a9f77de
nikhil <anc@gmail.com>
2023-10-22 18:28:30 +0530

    all

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,187 @@
+# module "catalogue_instance" {
+#   source  = "terraform-aws-modules/ec2-instance/aws"
+#   ami = data.aws_ami.devops.id
+#   instance_type = "t3.medium"
+#   vpc_security_group_ids = [data.aws_ssm_parameter.catalogue_sg_id.value]
+#   # it should be in Roboshop DB subnet
+#   subnet_id = element(split(",",data.aws_ssm_parameter.private_subnet_ids.value), 0)
+#   //user_data = file("catalogue.sh")
+#   tags = merge(
+#     {
+#         Name = "Catalogue-DEV-AMI"
+#     },
+#     var.common_tags
+#   )
+# }
+
+# resource "null_resource" "cluster" {
+#   # Changes to any instance of the cluster requires re-provisioning
+#   triggers = {
+#     instance_id = module.catalogue_instance.id
+#   }
+
+#   # Bootstrap script can run on any instance of the cluster
+#   # So we just choose the first in this case
+#   connection {
+#     type     = "ssh"
+#     user     = "centos"
+#     password = "DevOps321"
+#     host     = module.catalogue_instance.private_ip
+#   }
+
+#   #copy the file
+#   provisioner "file" {
+#     source      = "catalogue.sh"
+#     destination = "/tmp/catalogue.sh"
+#   }
+
+#   provisioner "remote-exec" {
+#     # Bootstrap script called with private_ip of each node in the cluster
+#     inline = [
+#       "chmod +x /tmp/catalogue.sh",
+#       "sudo sh /tmp/catalogue.sh ${var.app_version}"
+#     ]
+#   }
+# }
+
+# # stop instance to take AMI
+# resource "aws_ec2_instance_state" "catalogue_instance" {
+#   instance_id = module.catalogue_instance.id
+#   state       = "stopped"
+#   depends_on = [null_resource.cluster]
+# }
+
+
+# resource "aws_ami_from_instance" "catalogue_ami" {
+#   name               = "${var.common_tags.component}-${local.current_time}"
+#   source_instance_id = module.catalogue_instance.id
+#   depends_on = [ aws_ec2_instance_state.catalogue_instance ]
+# }
+
+# resource "null_resource" "delete_instance" {
+#   # Changes to any instance of the cluster requires re-provisioning
+#   triggers = {
+#     ami_id = aws_ami_from_instance.catalogue_ami.id
+#   }
+
+#   provisioner "local-exec" {
+#     # Bootstrap script called with private_ip of each node in the cluster
+#     command = "aws ec2 terminate-instances --instance-ids ${module.catalogue_instance.id}"
+#   }
+#   depends_on = [ aws_ami_from_instance.catalogue_ami ]
+# }
+
+# resource "aws_lb_target_group" "catalogue" {
+#   name     = "${var.project_name}-${var.common_tags.component}-${var.env}"
+#   port     = 8080
+#   protocol = "HTTP"
+#   vpc_id   = data.aws_ssm_parameter.vpc_id.value 
+#   deregistration_delay = 30
+#   health_check {
+#     enabled = true
+#     healthy_threshold = 2 #consider as healthy if 2 health checks are success 
+#     interval = 15 
+#     matcher = "200-299"
+#     path = "/health"
+#     port = 8080
+#     protocol = "HTTP"
+#     timeout = 5
+#     unhealthy_threshold = 3
+#   }
+# }
+
+# resource "aws_launch_template" "catalogue" {
+#   name = "${var.project_name}-${var.common_tags.component}-${var.env}"
+#   #here ami id should be the one we just created
+#   image_id = aws_ami_from_instance.catalogue_ami.id 
+
+#   instance_initiated_shutdown_behavior = "terminate"
+
+#   instance_type = "t2.micro"
+
+#   vpc_security_group_ids = [data.aws_ssm_parameter.catalogue_sg_id.value]   
+
+#   tag_specifications {
+#     resource_type = "instance"
+
+#     tags = {
+#       Name = "catalogue"
+#     }
+#   }
+#   # we dont need this since we already configured aws completely 
+#   # user_data = filebase64("${path.module}/catalogue.sh")
+# }
+
+# resource "aws_autoscaling_group" "catalogue" {
+#   name                      = "${var.project_name}-${var.common_tags.component}-${var.env}"
+#   max_size                  = 3 
+#   min_size                  = 2
+#   health_check_grace_period = 300
+#   health_check_type         = "ELB"
+#   desired_capacity          = 2
+#   target_group_arns = [aws_lb_target_group.catalogue.arn]
+#   launch_template {
+#     id      = aws_launch_template.catalogue.id 
+#     version = "$Latest"
+#   }
+
+#   vpc_zone_identifier       = split(",",data.aws_ssm_parameter.private_subnet_ids.value) 
+
+#   tag {
+#     key                 = "Name"
+#     value               = "catalogue"
+#     propagate_at_launch = true
+#   }
+
+#   timeouts {
+#     delete = "15m"
+#   }
+#   lifecycle {
+#     create_before_destroy = true 
+#   }
+# }
+
+# resource "aws_autoscaling_policy" "catalogue" {
+#   autoscaling_group_name = aws_autoscaling_group.catalogue.name  
+#   name                   = "cpu"
+#   policy_type            = "TargetTrackingScaling" 
+
+#   target_tracking_configuration {
+#     predefined_metric_specification {
+#       predefined_metric_type = "ASGAverageCPUUtilization"
+#     }
+
+#     target_value = 50.0
+#   }
+# }
+
+# resource "aws_lb_listener_rule" "catalogue" {
+#   listener_arn = data.aws_ssm_parameter.app_alb_listener_arn.value 
+#   priority     = 10
+
+#   action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.catalogue.arn 
+#   }
+#    condition {
+#     host_header {
+#       # for dev it should be app-dev and for prod is should be app-prod
+#       values = ["${var.common_tags.component}.app-${var.env}.${var.domain_name}"]
+#     }
+#   }
+# }
+
+# output "app_version" {
+#   value = var.app_version
+# }
+
 module "catalogue_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
-  ami = data.aws_ami.devops.id
+  ami = data.aws_ami.devops_ami.id
   instance_type = "t3.medium"
+  # vpc_security_group_ids = [module.mongodb_sg.security_group_id] 
   vpc_security_group_ids = [data.aws_ssm_parameter.catalogue_sg_id.value]
-  # it should be in Roboshop DB subnet
+  # this should be in roboshop db subnet
   subnet_id = element(split(",",data.aws_ssm_parameter.private_subnet_ids.value), 0)
   //user_data = file("catalogue.sh")
   tags = merge(
@@ -23,15 +201,14 @@ resource "null_resource" "cluster" {
   # Bootstrap script can run on any instance of the cluster
   # So we just choose the first in this case
   connection {
-    type     = "ssh"
-    user     = "centos"
+    type = "ssh"
+    user = "centos"
     password = "DevOps321"
-    host     = module.catalogue_instance.private_ip
+    host = module.catalogue_instance.private_ip
   }
-
-  #copy the file
+  # copy the file
   provisioner "file" {
-    source      = "catalogue.sh"
+    source = "catalogue.sh"
     destination = "/tmp/catalogue.sh"
   }
 
@@ -48,10 +225,10 @@ resource "null_resource" "cluster" {
 resource "aws_ec2_instance_state" "catalogue_instance" {
   instance_id = module.catalogue_instance.id
   state       = "stopped"
-  depends_on = [null_resource.cluster]
+  depends_on = [ null_resource.cluster ]
 }
 
-
+# take ami from existing instance
 resource "aws_ami_from_instance" "catalogue_ami" {
   name               = "${var.common_tags.component}-${local.current_time}"
   source_instance_id = module.catalogue_instance.id
@@ -75,12 +252,12 @@ resource "aws_lb_target_group" "catalogue" {
   name     = "${var.project_name}-${var.common_tags.component}-${var.env}"
   port     = 8080
   protocol = "HTTP"
-  vpc_id   = data.aws_ssm_parameter.vpc_id.value 
-  deregistration_delay = 30
+  vpc_id   = data.aws_ssm_parameter.vpc_id.value
+  deregistration_delay = 60
   health_check {
     enabled = true
-    healthy_threshold = 2 #consider as healthy if 2 health checks are success 
-    interval = 15 
+    healthy_threshold = 2 # consider as healthy if 2 health checks are success
+    interval = 15
     matcher = "200-299"
     path = "/health"
     port = 8080
@@ -92,59 +269,58 @@ resource "aws_lb_target_group" "catalogue" {
 
 resource "aws_launch_template" "catalogue" {
   name = "${var.project_name}-${var.common_tags.component}-${var.env}"
-  #here ami id should be the one we just created
-  image_id = aws_ami_from_instance.catalogue_ami.id 
 
+  #image created in this deployment
+  image_id = aws_ami_from_instance.catalogue_ami.id
   instance_initiated_shutdown_behavior = "terminate"
-
   instance_type = "t2.micro"
 
-  vpc_security_group_ids = [data.aws_ssm_parameter.catalogue_sg_id.value]   
+  vpc_security_group_ids = [data.aws_ssm_parameter.catalogue_sg_id.value]
 
   tag_specifications {
     resource_type = "instance"
 
     tags = {
-      Name = "catalogue"
+      Name = "Catalogue"
     }
   }
-  # we dont need this since we already configured aws completely 
-  # user_data = filebase64("${path.module}/catalogue.sh")
+  # not required, because we already prepared AMI
+  #user_data = filebase64("${path.module}/catalogue.sh")
 }
 
 resource "aws_autoscaling_group" "catalogue" {
-  name                      = "${var.project_name}-${var.common_tags.component}-${var.env}"
-  max_size                  = 3 
+  name                      = "${var.project_name}-${var.common_tags.component}-${var.env}-${local.current_time}"
+  max_size                  = 3
   min_size                  = 2
   health_check_grace_period = 300
   health_check_type         = "ELB"
   desired_capacity          = 2
   target_group_arns = [aws_lb_target_group.catalogue.arn]
   launch_template {
-    id      = aws_launch_template.catalogue.id 
+    id      = aws_launch_template.catalogue.id
     version = "$Latest"
   }
-
-  vpc_zone_identifier       = split(",",data.aws_ssm_parameter.private_subnet_ids.value) 
+  vpc_zone_identifier       = split(",",data.aws_ssm_parameter.private_subnet_ids.value)
 
   tag {
     key                 = "Name"
-    value               = "catalogue"
+    value               = "Catalogue"
     propagate_at_launch = true
   }
 
   timeouts {
     delete = "15m"
   }
+
   lifecycle {
-    create_before_destroy = true 
+    create_before_destroy = true
   }
 }
 
 resource "aws_autoscaling_policy" "catalogue" {
-  autoscaling_group_name = aws_autoscaling_group.catalogue.name  
+  autoscaling_group_name = aws_autoscaling_group.catalogue.name
   name                   = "cpu"
-  policy_type            = "TargetTrackingScaling" 
+  policy_type            = "TargetTrackingScaling"
 
   target_tracking_configuration {
     predefined_metric_specification {
@@ -156,16 +332,17 @@ resource "aws_autoscaling_policy" "catalogue" {
 }
 
 resource "aws_lb_listener_rule" "catalogue" {
-  listener_arn = data.aws_ssm_parameter.app_alb_listener_arn.value 
+  listener_arn = data.aws_ssm_parameter.app_alb_listener_arn.value
   priority     = 10
 
   action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.catalogue.arn 
+    target_group_arn = aws_lb_target_group.catalogue.arn
   }
-   condition {
+
+  condition {
     host_header {
-      # for dev it should be app-dev and for prod is should be app-prod
+      # for dev instance it should be app-dev, for prod it should be app-prod
       values = ["${var.common_tags.component}.app-${var.env}.${var.domain_name}"]
     }
   }
